5e27d3d4681dc2b45d59e360458810dfd035faaf
alarmshv150 <armashov7177@gmail.com>
2021-11-20 16:54:48 +0200

    delete default user navigation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <div class="app">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <div class="app"></div>
 </template>
 
 <style>
