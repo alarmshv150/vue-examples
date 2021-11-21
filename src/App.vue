485502e677ecc4ddd20c15605bf9ea28004c95ff
alarmshv150 <armashov7177@gmail.com>
2021-11-21 21:28:09 +0200

    create todo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="app">
-    <div v-for="elem in list" :key="elem.id">
-      {{ elem.id }} - {{ elem.name }}
+    <!-- prevent default browser behavior -->
+    <form @submit.prevent>
+      <h4>create todo</h4>
+      <!--model binding and input event handling-->
+      <input
+        :value="title"
+        @input="title = $event.target.value"
+        type="text"
+        placeholder="title"
+      />
+      <input
+        :value="completed"
+        @input="completed = $event.target.value"
+        type="text"
+        placeholder="completed"
+      />
+      <button @click="createTodo">create</button>
+    </form>
+    <h3>todos</h3>
+    <div v-for="todo in todos" :key="todo.id">
+      <h3>{{ todo.id }}</h3>
+      <div><strong>Title:</strong>{{ todo.title }}</div>
+      <div><strong>Completed:</strong>{{ todo.completed }}</div>
     </div>
   </div>
 </template>
@@ -10,12 +31,34 @@
 export default {
   data() {
     return {
-      list: [
-        { id: 1, name: "elem 1" },
-        { id: 2, name: "elem 2" },
-        { id: 3, name: "elem 3" },
+      todos: [
+        { id: 0, title: "todo 1", completed: "true" },
+        { id: 1, title: "todo 2", completed: "false" },
+        { id: 2, title: "todo 3", completed: "true" },
       ],
+      title: "",
+      completed: "",
     };
+  },
+  methods: {
+    createTodo() {
+      const newTodo = {
+        id: Date.now(),
+        title: this.title,
+        completed: this.completed,
+      };
+      this.todos.push(newTodo);
+      this.title = "";
+      this.completed = "";
+    },
+
+    /* input event handling
+    inputTitle(event) {
+      this.title = event.target.value;
+    },
+    inputComp(event) {
+      this.completed = event.target.value;
+    }, */
   },
 };
 </script>
@@ -30,3 +73,4 @@ export default {
   padding: 30px;
 }
 </style>
+
