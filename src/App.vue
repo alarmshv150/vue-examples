6614950b9416c716893c430d1ec75fcc6778b501
alarmshv150 <armashov7177@gmail.com>
2021-11-22 20:51:56 +0200

    computed props

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="app">
+    <h1>Counter:{{ counter }}</h1>
+    <button @click="counter--">Decrement</button>
+    <button @click="counter++">Increment</button>
+
+    <!-- incorrectLength() without computed -->
+    <h1>The number of elements:{{ incorrectLength }}</h1>
+    <div v-for="elem in list" :key="elem.id">{{ elem }}</div>
+    <button @click="list.push(list.length + 1)">Add element</button>
+
     <todo-form @create="createTodo" />
     <todo-list :todos="todos" />
   </div>
@@ -17,6 +26,9 @@ export default {
 
   data() {
     return {
+      counter: 0,
+      list: [1, 2],
+
       todos: [
         { id: 0, title: "todo 1", completed: "true" },
         { id: 1, title: "todo 2", completed: "false" },
@@ -29,6 +41,23 @@ export default {
       this.todos.push(todo);
     },
   },
+  /* called only with changing of the list length */
+  computed: {
+    incorrectLength() {
+      console.log("called");
+      return this.list.length + 1;
+    },
+  },
+
+  /* increment() {
+      this.counter++;
+    },
+    decrement() {
+      this.counter--;
+    },
+    addElement() {
+      this.list.push(this.list.length + 1);
+    }, */
 };
 </script>
 
